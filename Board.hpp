231b3978c49901099b4712c2723f432b302f2ee5
andriusezerskis <andrius.ezerskis@ulb.be>
2022-11-08 18:11:07 +0100

    board start

--- Board.hpp
+++ Board.hpp
@@ -1,29 +1,23 @@
-#include <map>
-#include <iostream>
-#include "Constantes.hpp"
 #include "Cell.hpp"
-#include "Wall.hpp"
+
 
 class Board
 {
 private:
-    Text textGameOver{"Game Over", {250, 250}, 90, fl_rgb_color(255, 0, 255)};
-    Text textYouWin{"You Win!", {250, 250}, 90, FL_GREEN};
-    vector<vector<Cell>> cells;
-    vector<Cell> neighbors(int x, int y);
-    int neighborBombCount(int x, int y);
-    void initialize();
+    std::vector<std::vector<Cell>> cells;
+    
     int matrix_elements[3] = {0, 1, 2};
-    Cellmatrix_objects = {new Cell(matrix_elements[0]), new Cell(matrix_elements[1]), new Cell(matrix_elements[2])}
+    Cell matrix_objects[3] = {Cell(matrix_elements[0]), Cell(matrix_elements[1]), Cell(matrix_elements[2])};
+    void initialize();
 
-    public : Board()
+public: 
+    Board()
     {
         initialize();
     }
-    bool bombExposed();
     bool solved();
     void draw();
     void mouseMove(Point mouseLoc);
     void mouseClick(Point mouseLoc);
     void keyPressed(int keyCode);
-};
+};                                        
--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@
 FLAGS=-std=c++17 -Wall -Wpedantic -D_GNU_SOURCE -Werror -lfltk
 COMPILER=g++
 
-main: Constantes.hpp MainWindow.o main.cpp Canvas.o Cell.o #Wall.o Player.o Board.o
-	${COMPILER} -o jeu Constantes.hpp MainWindow.o main.cpp Canvas.o Cell.o ${FLAGS}
+main: Constantes.hpp MainWindow.o main.cpp Board.o Cell.o #Wall.o Player.o Board.o
+	${COMPILER} -o jeu Constantes.hpp MainWindow.o main.cpp Board.o Cell.o ${FLAGS}
 
 run:
 	make main 
@@ -14,8 +14,8 @@ MainWindow.o:MainWindow.cpp MainWindow.hpp
 Cell.o: Cell.cpp Cell.hpp
 	${COMPILER} -c Cell.cpp ${FLAGS}
 
-Canvas.o: Canvas.cpp Canvas.hpp Player.o
-	${COMPILER} -c Canvas.cpp ${FLAGS}
+Board.o: Board.cpp Board.hpp Player.o
+	${COMPILER} -c Board.cpp ${FLAGS}
 
 
 #Player.o: Player.cpp Player.hpp
