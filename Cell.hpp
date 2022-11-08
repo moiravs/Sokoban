cdab9719264973ae4c2b4b0217ea34db867f718a
emily <moira.vanderslagmolen@ulb.be>
2022-11-08 16:56:44 +0100

    cell

---
+++ Cell.hpp
@@ -0,0 +1,21 @@
+#include "Constantes.hpp"
+struct Point
+{
+    int x, y;
+};
+
+class Rectangle
+{
+    Point center;
+    int w, h;
+    Fl_Color fillColor, frameColor;
+
+public:
+    Rectangle(Point center, int w, int h,
+              Fl_Color frameColor = FL_BLACK,
+              Fl_Color fillColor = FL_WHITE);
+    void draw();
+    void setFillColor(Fl_Color newFillColor);
+    void setFrameColor(Fl_Color newFrameColor);
+    bool contains(Point p);
+};
