6c9edff2cc96dc3dc4c5d505d2dbd4532c7ddc12
andriusezerskis <andrius.ezerskis@ulb.be>
2022-11-12 18:12:11 +0100

    board avancement

--- levraijeu/Board.hpp
+++ levraijeu/Board.hpp
@@ -1,18 +1,26 @@
+#include "Constants.hpp"
 #include <vector>
 #include <string>
 class Board
 {
 private:
     /* data */
+    struct tuple {
+        size_t x_axis;
+        size_t y_axis;
+    };
     std::vector<std::vector<int>> matrix;
+    std::vector<tuple> boxesPositions;
+    std::vector<tuple> correctBoxesPositions;
+    std::vector<std::vector<tuple>> teleportersPositions;
     int player_x;
     int player_y;
 
 public:
-    void move( int final_pos_x, int final_pos_y);
+    void move(int final_pos_x, int final_pos_y);
     bool check_move(int final_pos_x, int final_pos_y);
     std::vector<std::vector<int>> getBoard();
-        std::string readFileIntoString(std::string fileName);
+    std::string readFileIntoString(std::string fileName);
     void createBoard(std::string fileContent);
     void printBoard();
 };
