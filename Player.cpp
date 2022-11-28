0bd2e157d265cb4943d4a776f81b68da9c4e1acf
andriusezerskis <andrius.ezerskis@ulb.be>
2022-11-28 12:41:41 +0100

    box and player classes separated

---
+++ Player.cpp
@@ -0,0 +1,23 @@
+#include "Player.hpp"
+
+void Player::setX(int x)
+{
+    this->x = x;
+}
+void Player::setY(int y)
+{
+    this->y = y;
+}
+int Player::getX()
+{
+    return (this->x);
+}
+int Player::getY()
+{
+    return (this->y);
+}
+
+void Player::incrementSteps()
+{
+    stepsCount++;
+}
