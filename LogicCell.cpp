a68f035cf2693f149ac619b6f8f9686794fbefef
andriusezerskis <andrius.ezerskis@ulb.be>
2022-11-30 22:23:55 +0100

    nothing

--- LogicCell.cpp
+++ LogicCell.cpp
@@ -4,6 +4,6 @@ LogicCell::LogicCell(size_t line, size_t column, cellType type){
     switch (type){
         case Normal:
         case Teleportation:
-            LogicCell(this->line, this->column, Teleportation)
+            LogicCell(this->line, this->column, Teleportation);
     }
 }
